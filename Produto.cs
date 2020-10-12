30a4930b9c4a9e3d670c7a9afb610f0512c70b24
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-10-12 19:21:01 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_TarefaEncapsulamentoProperties
+{
+    class Produto
+    {
+        //Definição de variáveis
+        private string _nome;
+        public double _preco { get; private set; }
+        public int _quantidade { get; private set; }
+
+        //Construtores
+        public Produto(string nome, double preco,int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        //Aplicação do Properties - envolvendo GET e SET
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if(value!=null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace POO_TarefaEncapsulamentoProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto a=new Produto("PS5",6000.00,10);
+
+            a.Nome = "XBOX X";
+
+            Console.WriteLine("Nome do Produto: " + a.Nome);
+            Console.WriteLine("Preço do Produto: R$ " + a._preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Quantidade: " + a._quantidade + " Unidades");
+
+            Console.ReadKey();
+        }
+    }
+}
